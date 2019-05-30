5dafe91cc5b5a9b2d430f915816cf704e4bc5032
Thomas Ryan <tomryan_8_1997@hotmail.com>
2019-05-29 21:10:34 -0400

    Added stability changes

---
+++ client.lua
@@ -0,0 +1,184 @@
+local socket = require('socket')
+
+local HOST, PORT, USERNAME, COLOUR
+local RECONATMP = 0
+
+file = io.open("info.txt", 'r+')
+if not file then
+	file = io.open('info.txt', 'w+')
+	un = " "
+	ip = " "
+	pt = "50001"
+	cl = "#000000"
+	file:write(un .. "," .. ip .. "," .. pt .. "," .. cl)
+else
+	info = {}
+	for str in string.gmatch(file:read(), "([^,]+)") do
+		table.insert(info, str)
+	end
+	un = info[1]
+	ip = info[2]
+	pt = info[3]
+	cl = info[4]
+end
+file:close()
+
+-- Add a line to the output. Inserts a timestamp to the string
+-- Taken directly from bizhawk co-op.lua
+function printOutput(str)
+	local text = forms.gettext(output)
+	local pos = #text
+	forms.setproperty(output, "SelectionStart", pos)
+
+	str = string.gsub (str, "\n", "\r\n")
+	str = "[" .. os.date("%H:%M:%S", os.time()) .. "] " .. str
+	if pos > 0 then
+		str = "\r\n" .. str
+	end
+
+	forms.setproperty(output, "SelectedText", str)
+end
+
+-- Keepalive function. Sends a ping every second telling the server it's still here
+function keepalive ()
+  start = os.time()
+  while true do
+    now = os.time()
+    if os.difftime(now, start) >= 1 then
+      pong = client:send("ping,")
+      if not pong then
+        printOutput("Disconnected from server")
+        HOST, PORT, USERNAME, COLOUR = nil
+				client:close()
+				forms.setproperty(connect, 'Enabled', true)
+				forms.setproperty(disconnect, 'Enabled', false)
+        coroutine.yield(true)
+      else
+	      s, status, partial = client:receive("*l")
+	      start = now
+			end
+    end
+    coroutine.yield(false)
+  end
+end
+
+-- Grab initial starting position
+local currentLocation = mainmemory.read_u16_be(0x1C8544)
+
+-- Variables for use later when I want to get pintpoint accuracy
+-- local respawnLocation = mainmemory.read_u16_be(0x11B948)
+-- local x = mainmemory.read_u16_be(0x1DAA54)
+-- local y = mainmemory.read_u16_be(0x1DAA58)
+-- local z = mainmemory.read_u16_be(0x1DAA5C)
+
+-- local inventory = mainmemory.readbyterange(0x11A644, 24)
+-- print(inventory)
+
+function Connect (args)
+  HOST = forms.gettext(host)
+  HOST = HOST:gsub(",", "")
+  PORT = forms.gettext(port)
+  PORT = PORT:gsub(",", "")
+  USERNAME = forms.gettext(username)
+  USERNAME = USERNAME:gsub(",", "")
+  COLOUR = forms.gettext(colour)
+  COLOUR = COLOUR:gsub(",", "")
+  COLOUR = COLOUR:gsub("ping", "")
+	file = io.open("info.txt", 'w')
+	file:write(USERNAME .. "," .. HOST .. "," .. PORT .. "," .. COLOUR)
+	file:close()
+  forms.setproperty(connect, 'Enabled', false)
+  forms.setproperty(disconnect, 'Enabled', true)
+end
+
+function Disconnect (args)
+	disconn = true
+	HOST, PORT, USERNAME, COLOUR = nil
+	printOutput("Disconnected from server")
+  forms.setproperty(connect, 'Enabled', true)
+  forms.setproperty(disconnect, 'Enabled', false)
+end
+
+-- GUI
+mainform = forms.newform(370, 240, "Map Watcher")
+username = forms.textbox(mainform, un, 90, 20, nil, 70, 20, false, false)
+host = forms.textbox(mainform, ip, 90, 20, nil, 70, 50, false, false)
+port = forms.textbox(mainform, pt, 90, 20, nil, 70, 80, false, false)
+colour = forms.textbox(mainform, cl, 90, 20, nil, 70, 110, false, false)
+lblUsername = forms.label(mainform, "Username:", 10, 22)
+lblHost = forms.label(mainform, "Host IP:", 22, 52)
+lblPort = forms.label(mainform, "Port:", 38, 82)
+lblColour = forms.label(mainform, "Colour:", 27, 112)
+output = forms.textbox(mainform, "", 170, 170, nil, 170, 20, true, true, 'Vertical')
+forms.setproperty(output, "ReadOnly", true)
+connect = forms.button(mainform, "Connect", Connect, 10, 140, 151, 20)
+disconnect = forms.button(mainform, "Disconnect", Disconnect, 10, 170, 151, 20)
+forms.setproperty(disconnect, 'Enabled', false)
+
+-- Checks to see if connection information is provided
+function checkInfo ()
+  while true do
+    -- print("Here")
+    if not HOST and not PORT and not USERNAME and not COLOUR then
+      emu.frameadvance()
+			coroutine.yield(false)
+    else
+      -- Create the client and connect
+      printOutput("Attempting to connect to server...")
+      client, err = socket.connect(HOST, PORT)
+      coroutine.yield(true)
+    end
+  end
+end
+
+-- Create the coroutine for the keepalive function
+pingRoutine = coroutine.create(keepalive)
+-- Create the coroutine for the checkRoutine function
+checkRoutine = coroutine.create(checkInfo)
+
+-- Main loop
+while true do
+	disconn = false
+  status, retInfo = coroutine.resume(checkRoutine)
+	if retInfo then
+		-- If the socket couldn't connect, print and message and end everything
+		if err then
+			printOutput("Cannot connect to " .. HOST .. " on port " .. PORT)
+			HOST, PORT, USERNAME, COLOUR = nil
+		  forms.setproperty(connect, 'Enabled', true)
+		  forms.setproperty(disconnect, 'Enabled', false)
+		else
+			printOutput("Connected to server!")
+			-- Send the identifying information to the server and start the main loop
+			client:send("username," .. USERNAME .. "," .. currentLocation .. "," .. COLOUR)
+		end
+	  while true do
+	    -- Start the keepalive routine
+	    status, check = coroutine.resume(pingRoutine)
+	    if check then
+				-- Reconnection code WIP
+					-- client, err = socket.connect(HOST, PORT)
+					-- if err then
+					-- 	RECONATMP = RECONATMP + 1
+					-- 	printOutput("Attempting to reconnect (" .. RECONATMP .. "/3)")
+					-- end
+					-- emu.frameadvance()
+					-- if RECONATMP == 3 then
+					-- 	break
+					-- end
+	      break
+	    end
+			if disconn then
+				break
+			end
+	    -- Send the server the user's location each time it changes
+	    if currentLocation ~= mainmemory.read_u16_be(0x1C8544) then
+	      currentLocation = mainmemory.read_u16_be(0x1C8544)
+	      if currentLocation <= 109 then
+	        client:send("location," .. currentLocation)
+	      end
+	    end
+	    emu.frameadvance()
+	  end
+	end
+end
--- server.py
+++ server.py
@@ -1,89 +1,129 @@
+import socket, threading, hashlib, time, os
 from flask import Flask, request, render_template, Response
-from flask_socketio import SocketIO, send, emit, join_room, leave_room
-import random, hashlib, time, json
+import socketio
 
+# Trying to keep console clutter to a minimum
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+# Holds all the information of the connected players
+PLAYERS = {}
+
+# Setup stuff for the web server
 app = Flask(__name__, static_url_path="")
-# Remember to change this... Or remove it entirely actually
-app.config['SECRET_KEY'] = "A4A4BB55B152833E72E5F3B85CAA30F61E99D4260063F723E7993323A6AC95B6"
-app.secret_key = "A4A4BB55B152833E72E5F3B85CAA30F61E99D4260063F723E7993323A6AC95B6"
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-socketio = SocketIO(app)
+sio = socketio.Server(async_mode='threading')
+app.wsgi_app = socketio.Middleware(sio, app.wsgi_app)
 
-import csv
+# Function for the keepalive thread
+def keepalive(conn, addr):
+    with conn:
+        conn.settimeout(3)
+        # Create a unique player hash ID
+        playerHash = hashlib.sha256(str(time.time).encode()).hexdigest() + hashlib.sha256(str(hash(conn)).encode()).hexdigest()
+        playerHash = hashlib.sha256(playerHash.encode()).hexdigest()
+        PLAYERS[playerHash] = {}
 
-with open("Locations.csv") as fil:
-    reader = csv.reader(fil)
-    LOCATIONS = []
-    for row in reader:
-        LOCATIONS.append(row[1])
+        # Function loop
+        while True:
+            try:
+                # Wait until data is received and then deal with it
+                data = conn.recv(1024)
+                if not data: break
+                message = data.split(b',')
+                # If ping send pong, works to ensure the connection is still good
+                if message[0] == b'ping':
+                    conn.sendall(b'pong' + b'\n')
+                # If the client says the player's location has changed, update the PLAYERS dict and send it to the webserver
+                if message[0] == b'location':
+                    PLAYERS[playerHash]['Location'] = int(message[1])
+                    sio.emit("updateMap",
+                    {
+                        "id": playerHash,
+                        "location": PLAYERS[playerHash]['Location']
+                    })
+                # Used to set the initial data for the player on first connection, sends data to webserver
+                if message[0] == b'username':
+                    PLAYERS[playerHash]['Username'] = message[1].decode()
+                    PLAYERS[playerHash]['Location'] = message[2].decode()
+                    PLAYERS[playerHash]['Colour'] = message[3].decode()
+                    print('User has connected:', PLAYERS[playerHash]['Username'])
+                    sio.emit("socketConnected",
+                    {
+                        "id": playerHash,
+                        "username": PLAYERS[playerHash]['Username'],
+                        "location": PLAYERS[playerHash]['Location'],
+                        "colour": PLAYERS[playerHash]['Colour']
+                    })
+            # If there's an error of any type, break out of the loop and kill the thread
+            except Exception as e:
+                print(e)
+                break
+        # Send a disconnected message and clean up the player's data
+        sio.emit("socketDisconnected", {"id": playerHash})
+        print(PLAYERS[playerHash]['Username'], "has disconnected")
+        del PLAYERS[playerHash]
 
-sessions = {}
+# Listens for connections to the server
+def listenForConnections():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.bind((HOST, PORT))
+        # When a new connection is made, create a new keepalive thread for it
+        while True:
+            sock.listen()
+            conn, addr = sock.accept()
+            t = threading.Thread(target=keepalive, args=(conn, addr))
+            t.start()
 
-# @socketio.on('my event')
-# def my_event(data):
-#     print("Connected: {}".format(data))
-
-@app.route("/login", methods=['GET'])
-def login():
-    if request.method == 'GET':
-        # id = request.args.get('id', type=float)
-        # id = 1
-        id = hashlib.sha256(str(time.time).encode()).hexdigest() + hashlib.sha256(str(random.randint(0,1000000000)).encode()).hexdigest()
-        id = hashlib.sha256(id.encode()).hexdigest()
-        sessions[str(id)] = {}
-        sessions[str(id)]['username'] = request.args.get('username', type=str)
-        sessions[str(id)]['password'] = request.args.get('password', type=str)
-        sessions[str(id)]['roomname'] = request.args.get('roomname', type=str)
-        sessions[str(id)]['location'] = request.args.get('location', type=int)
-        sessions[str(id)]['colour'] = "rgb({}, {}, {})".format(
-            random.randint(0,255),
-            random.randint(0,255),
-            random.randint(0,255)
-        )
-        socketio.emit("createdot", {
-            'player': sessions[str(id)]['username'],
-            'locationNum': request.args.get('location', type=int),
-            'id': id,
-            'colour': sessions[str(id)]['colour']
-        })
-        # print(session['username'], session['password'], session['roomname'])
-
-        return Response(id, status=200)
-
-@app.route("/updatemap", methods=['GET'])
-def updatemap():
-    print(sessions)
-    id = request.args.get('id', type=str)
-    if 'username' not in sessions[str(id)]:
-        print('No username')
-        return Response(None, status=404)
-    if 'password' not in sessions[str(id)]:
-        print('No password')
-        return Response(None, status=404)
-    if 'roomname' not in sessions[str(id)]:
-        print('No roomname')
-        return Response(None, status=404)
-
-    print(id)
-
-    if request.method == 'GET':
-        sessions[str(id)]['location'] = request.args.get('location', type=int)
-        locationNum = request.args.get('location', type=int)
-    socketio.emit("updatemap", {
-        'player': sessions[str(id)]['username'],
-        'locationNum': locationNum,
-        'id': id
-    })
-    # return "You've entered {}".format(LOCATIONS[locationNum])
-    return Response(None, status=200)
-
-@app.route('/getsession', methods=['GET'])
-def getsession():
-    return Response(json.dumps(sessions), status=200, mimetype='application/json')
-
+# Render the main html page
 @app.route('/')
 def index():
     return render_template("index.html")
 
+# Send the entire player list to the webserver
+@sio.on('getMap')
+def my_event(data):
+    sio.emit('sendMap', PLAYERS)
+
+# Main function
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0')
+    # Get Host and Port either from user input or file if exists
+    if os.path.exists("serverInfo.txt"):
+        print("serverInfo.txt found. Loading saved IP Address and Port...")
+        with open("serverInfo.txt") as fil:
+            HOST, PORT = fil.read().split(",")[:2]
+        PORT = int(PORT)
+        print("Host and port loaded...\n{}:{}".format(HOST,PORT))
+    else:
+        try:
+            HOST = HOST = input("Enter your IP Address (Default 0.0.0.0): ")
+            if HOST == "":
+                HOST = '0.0.0.0'
+        except:
+            HOST = '0.0.0.0'
+        try:
+            PORT = int(input("Enter your desired port (Default 50001): "))
+            if PORT == "":
+                PORT = 50001
+        except:
+            PORT = 50001
+        decision = input("Save {}:{} for later use? ".format(HOST,PORT))
+        if decision.lower() in ['y', 'yes', 'yeah', 'ye', 'oui']:
+            with open("serverInfo.txt", 'w') as fil:
+                fil.write("{},{}".format(HOST,PORT))
+            print("Server info saved for later reuse...\nStarting server now...")
+
+    print("")
+
+    # Start server thread
+    SERVER = threading.Thread(target=listenForConnections)
+    SERVER.start()
+
+    # Disable logging
+    app.logger.disabled = True
+    log = logging.getLogger('werkzeug')
+    log.disabled = True
+
+    # Run Flask
+    app.run(threaded=True, host='0.0.0.0', port=80)
